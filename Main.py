c7f0e8429846328a469f649aa1b5d719d92119d9
m9ts <mat3us.gois@gmail.com>
2025-08-05 18:59:00 -0300

    Iniciando projeto

---
+++ Main.py
@@ -0,0 +1,18 @@
+class Main:
+    pass
+
+print("Testando o projeto")
+
+from Cliente import Cliente
+
+from Conta import Conta
+
+c1 = Cliente("Teodoro", "11111-22222")
+conta = Conta(c1.nome, 10101, 0)
+print("Nome do titular da conta: ", conta.titular, " | Número da conta: ", conta.numero, " | Saldo: ", conta.saldo)
+
+# Imprime o ID do objeto
+print(c1)
+
+# Imprime o conteúdo adicionado
+print("Nome: ", c1.nome, " | ", "Telefone:", c1.telefone)
