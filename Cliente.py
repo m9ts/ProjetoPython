178e048b05e80da5e0b9d5fa42b9ee6288671e3c
m9ts <mat3us.gois@gmail.com>
2025-08-06 10:51:06 -0300

    Projeto finalizado
    # Conflicts:
    #	Conta.py
    #	Main.py

--- Cliente.py
+++ Cliente.py
@@ -1,4 +1,31 @@
 class Cliente:
-    def __init__(self, n, fone):
-        self.nome = n
-        self.telefone = fone
+    def __init__(self, nome, telefone):
+        self._nome = nome
+        self._telefone = telefone
+
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome
+
+    @property
+    def telefone(self):
+        return self._telefone
+
+    @telefone.setter
+    def telefone(self, novo_telefone):
+        self._telefone = novo_telefone
+
+
+
+# Método Get
+# def get_nome(self):
+#     return self._nome
+
+# Método Set
+# def set_nome(self, nome):
+#     self._nome = nome
